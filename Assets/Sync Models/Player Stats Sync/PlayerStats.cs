701f5150d7d993ba8de0d4d761bb74c0ade6c0c6
sanidk <44160946+sanidk@users.noreply.github.com>
2021-10-28 16:02:21 +0200

    asd

--- Assets/Sync Models/Player Stats Sync/PlayerStats.cs
+++ Assets/Sync Models/Player Stats Sync/PlayerStats.cs
@@ -44,7 +44,7 @@ public class PlayerStats : MonoBehaviour
 
         if (Application.platform != RuntimePlatform.Android)
         {
-            //Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
 
