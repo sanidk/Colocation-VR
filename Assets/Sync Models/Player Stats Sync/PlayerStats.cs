73c83429f4a74c3e5e65ebfb173616167ddb3381
sanidk <44160946+sanidk@users.noreply.github.com>
2021-11-01 12:14:44 +0100

    removed destroy

--- Assets/Sync Models/Player Stats Sync/PlayerStats.cs
+++ Assets/Sync Models/Player Stats Sync/PlayerStats.cs
@@ -53,7 +53,7 @@ public class PlayerStats : MonoBehaviour
 
         if (Application.platform != RuntimePlatform.Android)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
         
     }
