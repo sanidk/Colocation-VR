b326a114b1485241fe767bfe7e180ab29f8b8006
Bjørn Winther <bwinth18@student.aau.dk>
2021-11-02 13:53:37 +0100

    diller

--- Assets/Sync Models/Player Stats Sync/PlayerStats.cs
+++ Assets/Sync Models/Player Stats Sync/PlayerStats.cs
@@ -153,10 +153,11 @@ public class PlayerStats : MonoBehaviour
         if (cp.thisCollider.name == "TorsoCollider" && collision.collider.CompareTag("Bullet"))
         {
             //_health -= 10;
-            //hp -= 10;
+            hp -= 10;
             //_playerStatsSync.SetHealth(_playerStatsSync.GetHealth() - 10);
-            _health -= 10;
-            _playerStatsSync.SetHealth(_health);
+            //_health -= 10;
+            //_playerStatsSync.SetHealth(_health);
+            _playerStatsSync.SetHealth(hp);
         }/*
         if (cp.thisCollider.name == "RightThighCollider" && collision.collider.CompareTag("Bullet") || cp.thisCollider.name == "LeftThighCollider"
             && collision.collider.CompareTag("Bullet"))
