48dec25829625a41e401db46c5cc586bea09c6de
DonMiguel95 <ml.krarup@hotmail.com>
2021-12-02 10:40:12 +0100

    walkingsound?

--- Assets/Animations/VRAnimatorController.cs
+++ Assets/Animations/VRAnimatorController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class VRAnimatorController : MonoBehaviour
 {
+    private AudioSource footstepSound;
     private Animator animator;
     public Vector3 previousPos;
+
     public float velocity;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        footstepSound = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -20,8 +24,16 @@ public class VRAnimatorController : MonoBehaviour
         velocity = ((transform.TransformPoint(Vector3.zero) - previousPos).magnitude) / Time.deltaTime;
         previousPos = transform.TransformPoint(Vector3.zero);
 
-        animator.SetBool("isMoving", velocity > 0.5f);
+        animator.SetBool("isMoving", velocity > 0.3f);
 
+        if (velocity > 0.3f)
+        {
+            footstepSound.Play();
+        }
+        else
+        {
+            footstepSound.Stop();
+        }
 
         Debug.Log("Velocity: " + velocity);
     }
