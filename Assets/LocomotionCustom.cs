00e999840e4cf1cc6a89d5ca5e7a9e354118747a
Bjørn Winther <bwinth18@student.aau.dk>
2021-12-06 14:16:47 +0100

    dsa

--- Assets/LocomotionCustom.cs
+++ Assets/LocomotionCustom.cs
@@ -37,13 +37,13 @@ public class LocomotionCustom : MonoBehaviour
 
             if (triggerValue.x > 0)
             {
-                //transform.position = new Vector3(transform.position.x + (triggerValue.x * scalar), transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x + (triggerValue.x * scalar), transform.position.y, transform.position.z);
                 //transform.position += Vector3.right;
 
             }
             if (triggerValue.x < 0)
             {
-                //transform.position = new Vector3(transform.position.x + (triggerValue.x * scalar), transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x + (triggerValue.x * scalar), transform.position.y, transform.position.z);
 
             }
 
